0a8e29a6aa1da385d420e834a5214149d439dd11
qiaoxs <qqiao32@gmail.com>
2021-07-15 01:07:16 +0800

    [DTS]002100070015 [Description]增加了一个学生类 [Author]qiaoxiaosong

---
+++ TestMoudle01/src/com/qiaoxiaosong/Student.java
@@ -0,0 +1,13 @@
+package com.qiaoxiaosong;
+
+/**
+ * Created with IntelliJ IDEA.
+ *
+ * @Auther: qiaoxiaosong
+ * @Date: 2021/07/15/1:05
+ * @Description:
+ */
+public class Student {
+    private String name;
+    private int score;
+}
